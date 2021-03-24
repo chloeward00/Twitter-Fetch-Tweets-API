74d0a0eeeb7ed2f312e80b5cbf2d6578b45755bd
Chloe Ward <45489389+chloeward00@users.noreply.github.com>
2021-03-24 15:21:40 +0000

    Update config.js

--- twitter-api/config.js
+++ twitter-api/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     twitter: {
-      consumer_key: 'FuAP6sEa1ZDppZf6LI9UugQuS',
-      consumer_secret: 'b7tojxlGIdvl8wGqkOAAWCwpHtaAQ5ktCPlCyEwwe9cvJPZY4r',
-      access_token_key: '1066547785-TeDI8q90dwRJWT9JMm4YlBetjmQhKN0v93ekhJy',
-      access_token_secret: 'dnoUD7C3qThMj0yZoxRCQoBkgiZE6XPmuGMLH3zjBNOCL'
+      consumer_key: 'enter your consumer key',
+      consumer_secret: 'enter your consumer secret',
+      access_token_key: 'enter your access token key',
+      access_token_secret: 'enter your access_token_secret'
     }
   }
